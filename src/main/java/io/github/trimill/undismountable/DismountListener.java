d6a5d175cb2a065f76e07aed1039d40f9c909491
trimill <trimill012@gmail.com>
2021-10-22 17:14:39 -0400

    Initial commit

---
+++ src/main/java/io/github/trimill/undismountable/DismountListener.java
@@ -0,0 +1,51 @@
+package io.github.trimill.undismountable;
+
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.util.Location;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.spigotmc.event.entity.EntityDismountEvent;
+
+public class DismountListener implements Listener {
+    // This is called whenever an entity dismounts from another entity
+    @EventHandler
+    public void onDismount(EntityDismountEvent event) {
+        // If the dismounter is a player and the mount is not dead
+        if (event.getEntity() instanceof Player && !event.getDismounted().isDead()) {
+            // Get the region that the dismount occurs in
+            RegionQuery query = Undismountable.CONTAINER.createQuery();
+            Location loc = BukkitAdapter.adapt(event.getEntity().getLocation());
+            ApplicableRegionSet regionSet = query.getApplicableRegions(loc);
+
+            // Get the state of the vehicle-dismount flag for this player
+            Player player = (Player)(event.getEntity());
+            LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
+            StateFlag.State state = regionSet.queryState(localPlayer, Undismountable.FLAG_VEHICLE_DISMOUNT);
+
+            // If the flag is DENY, prevent the player from dismounting
+            if (state == StateFlag.State.DENY) {
+                event.setCancelled(true);
+                // Send a dismount error message if configured
+                if (Undismountable.DISMOUNT_DENIED_MESSAGE != null
+                 && Undismountable.DISMOUNT_DENIED_MESSAGE.length() > 0
+                ) {
+                    // Prevent repeat messages by checking the difference between
+                    // the current time and the last time this player was sent an error
+                    long currentTime = System.currentTimeMillis();
+                    long lastErrorTime = Undismountable.LastErrorMessage.getOrDefault(player.getUniqueId(), 0l);
+                    if(currentTime >= lastErrorTime + Undismountable.ERROR_TIMEOUT) {
+                        // Send the message and set the last dismount time
+                        player.sendMessage(Undismountable.DISMOUNT_DENIED_MESSAGE);
+                        Undismountable.LastErrorMessage.put(player.getUniqueId(), currentTime);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/io/github/trimill/undismountable/Undismountable.java
@@ -0,0 +1,62 @@
+package io.github.trimill.undismountable;
+
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.logging.Level;
+
+public final class Undismountable extends JavaPlugin {
+    public static RegionContainer CONTAINER;
+    // WorldGuard flag to set whether players can dismount in a region
+    public static StateFlag FLAG_VEHICLE_DISMOUNT = null;
+    // The error message to send when dismounting is denied, send nothing if null or empty string
+    public static String DISMOUNT_DENIED_MESSAGE = null;
+    // The timeout between repeated error messages (ms)
+    public static long ERROR_TIMEOUT = 1000;
+    // When did each player last recieve an error message?
+    public static HashMap<UUID, Long> LastErrorMessage;
+
+    @Override
+    public void onEnable() {
+        // Initialize constants and register the dismount event listener
+        LastErrorMessage = new HashMap<>();
+        CONTAINER = WorldGuard.getInstance().getPlatform().getRegionContainer();
+        getServer().getPluginManager().registerEvents(new DismountListener(), this);
+
+        // Load from the config file
+        saveDefaultConfig();
+        FileConfiguration cfg = getConfig();
+        DISMOUNT_DENIED_MESSAGE = cfg.getString("dismount_message");
+        ERROR_TIMEOUT = cfg.getLong("error_timeout", 1000);
+    }
+
+    @Override
+    public void onLoad() {
+        // Attempt to register the "vehicle-dismount" flag, setting FLAG_VEHICLE_DISMOUNT if successful
+        FlagRegistry registry = WorldGuard.getInstance().getFlagRegistry();
+        try {
+            StateFlag flag = new StateFlag("vehicle-dismount", true);
+            registry.register(flag);
+            FLAG_VEHICLE_DISMOUNT = flag;
+        } catch (FlagConflictException e) {
+            Flag<?> existing = registry.get("vehicle-dismount");
+            if (existing instanceof StateFlag) {
+                FLAG_VEHICLE_DISMOUNT = (StateFlag) existing;
+            } else {
+                getLogger().log(Level.WARNING, "Flag name 'vehicle-dismount' is already taken, Undismountable cannot load.");
+            }
+        }
+    }
+
+    public static Undismountable inst() {
+        return getPlugin(Undismountable.class);
+    }
+}
